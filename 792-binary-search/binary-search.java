7944a4a56627df9d33ffef0e891c5c9ec582515c
BubalanShanmugam <bubalanshanmugam62@gmail.com>
2025-05-29 23:41:31 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (7.72%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,17 +1,33 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int low =0;
-        int high = nums.length-1;
+        // int low =0;
+        // int high = nums.length-1;
 
-        while(low<= high){
-            int mid = (low+high)/2;
+        // while(low<= high){
+        //     int mid = (low+high)/2;
 
-            if(nums[mid]== target) return mid;
+        //     if(nums[mid]== target) return mid;
 
-            else if(nums[mid]< target) low = mid+1;
+        //     else if(nums[mid]< target) low = mid+1;
 
-            else{ high = mid-1;}
-        }
-        return -1;
+        //     else{ high = mid-1;}
+        // }
+        // return -1;
+
+
+
+        //using recursion
+
+        return bs(nums, 0, nums.length-1, target);
+    }
+
+    public int bs(int[] arr, int low, int high, int target){
+        if(low> high) return -1;
+        int mid = (low+high)/2;
+        if(arr[mid]==target) return mid;
+
+        else if(arr[mid]< target) return bs(arr, mid+1, high, target);
+
+        else return bs(arr, low, mid-1, target);
     }
 }
