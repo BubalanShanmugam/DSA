6955b61460e9b24f78d087a45a2cf2c681cc90ce
BubalanShanmugam <bubalanshanmugam62@gmail.com>
2025-06-02 11:37:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Square Root/square-root.java
@@ -0,0 +1,26 @@
+/*You are required to complete
+this function*/
+
+// Function to find square root
+// x: element to find square root
+class Solution {
+    int floorSqrt(int n) {
+        // Your code here
+        int low=1;
+        int high = n;
+        int ans = 1;
+        
+        while(low<=high){
+            int mid = (low + high)/2;
+            
+            if(mid*mid <= n){
+                ans = mid;
+                low= mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return ans;
+    }
+}
