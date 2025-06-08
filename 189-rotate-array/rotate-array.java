ff4ce89fe002fb22c4f714c03beffceb340ad6fa
BubalanShanmugam <bubalanshanmugam62@gmail.com>
2025-06-08 11:01:34 +0530

    Time: 0 ms (100.00%) | Memory: 57.3 MB (77.52%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -15,28 +15,50 @@ class Solution {
         //     nums[i] = arr[i];
         // }
 
+// ----------------------------------------------------------
 
-        int n = nums.length;
-        k = k % n;
+        // int n = nums.length;
+        // k = k % n;
+        // int[] arr = new int[n];
+        // int j =0;
+        // for (int i = n - k; i < n ; i++) {
+        //     arr[j] = nums[i];
+        //     j++;
+        // }
 
-        int[] arr = new int[n];
-        int j =0;
-        for (int i = n - k; i < n ; i++) {
-            arr[j] = nums[i];
-            j++;
-        }
-
-        int l = 0;
-        for (int a = n-k-1; a >= 0; a--) {
-            nums[n - 1 - l] = nums[a];
-            l++;
-        }
+        // int l = 0;
+        // for (int a = n-k-1; a >= 0; a--) {
+        //     nums[n - 1 - l] = nums[a];
+        //     l++;
+        // }
        
        
-        int z=0;
-        for(int i =0; i<k;i++){
-            nums[z] = arr[i];
-            z++;
+        // int z=0;
+        // for(int i =0; i<k;i++){
+        //     nums[z] = arr[i];
+        //     z++;
+        // }
+
+        // ------------------------------------------
+
+            int n = nums.length;
+            k = k % n;
+
+            reverse(nums, 0, n-k-1);
+
+            reverse(nums, n-k, n-1);
+
+            reverse(nums, 0, n-1);
+
+    } 
+
+     static void reverse(int [] arr, int start, int end){
+        while(start<end){
+            int temp = arr[start];
+            arr[start]= arr[end];
+            arr[end]= temp;
+            start++;
+            end--;
         }
     }
 }
