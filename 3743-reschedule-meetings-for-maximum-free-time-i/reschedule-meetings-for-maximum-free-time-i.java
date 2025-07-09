1ce8883725f32b4e344723a701abd923f55e7aec
BubalanShanmugam <bubalanshanmugam62@gmail.com>
2025-07-09 20:22:17 +0530

    Time: 1 ms (100.00%) | Memory: 63.4 MB (40.00%) - LeetSync

---
+++ 3743-reschedule-meetings-for-maximum-free-time-i/reschedule-meetings-for-maximum-free-time-i.java
@@ -0,0 +1,67 @@
+// class Solution {
+//     public int maxFreeTime(int eventTime, int k, int[] startTime, int[] endTime) {
+//         int event =0;
+//         for(int i=0;i<endTime.length;i++){
+//             event=event+(endTime[i]-startTime[i]);
+//         }
+
+//         return eventTime - event;
+//     }
+// }
+
+
+
+
+// public class Solution {
+
+//     public int maxFreeTime(
+//         int eventTime,
+//         int k,
+//         int[] startTime,
+//         int[] endTime
+//     ) {
+//         int n = startTime.length;
+//         int res = 0;
+//         int[] sum = new int[n + 1];
+//         for (int i = 0; i < n; i++) {
+//             sum[i + 1] = sum[i] + endTime[i] - startTime[i];
+//         }
+//         for (int i = k - 1; i < n; i++) {
+//             int right = i == n - 1 ? eventTime : startTime[i + 1];
+//             int left = i == k - 1 ? 0 : endTime[i - k];
+//             res = Math.max(res, right - left - (sum[i + 1] - sum[i - k + 1]));
+//         }
+//         return res;
+//     }
+// }
+
+
+
+
+
+class Solution {
+    public int maxFreeTime(int eventTime, int k, int[] startTime, int[] endTime) {
+        int n = startTime.length;
+        int res = 0, left = 0, blocked = 0;
+
+        for (int i = 0; i < n; i++) {
+            res = Math.max(res, (startTime[i] - left) - blocked);
+            blocked += endTime[i] - startTime[i];
+
+            if (i >= k) {
+                int j = i - k;
+                left = endTime[j];
+                blocked -= endTime[j] - startTime[j];
+            }
+        }
+
+        res = Math.max(res, (eventTime - left) - blocked);
+        return res;
+    }
+}
+
+
+
+
+
+
