9b938ad69614f2711dc0d568b70c96d44f6a9bfa
BUBALAN S <bubalanshanmugam62@gmail.com>
2025-07-24 00:00:53 +0530

    Time: 3 ms (40.77%) | Memory: 40.4 MB (57.56%) - LeetSync

--- 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.java
+++ 2649-count-total-number-of-colored-cells/count-total-number-of-colored-cells.java
@@ -1,6 +1,19 @@
 class Solution {
     public long coloredCells(int n) {
         // return (long) Math.pow(n,2)+n-1;
-        return 1+(long)n*(n-1)*2;
+
+
+
+        // return 1+(long)n*(n-1)*2;
+
+
+
+        long color =1;
+        int ad=4;
+        while(--n>0){
+            color +=ad;
+            ad+=4;
+        }
+        return color;
     }
 }
