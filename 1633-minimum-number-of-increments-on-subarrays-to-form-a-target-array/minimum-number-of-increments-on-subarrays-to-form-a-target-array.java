3f6881d34e9d11446ba902a42a976207cba94a22
BUBALAN S <bubalanshanmugam62@gmail.com>
2025-10-30 12:41:16 +0530

    Time: 4 ms (44.79%) | Memory: 57 MB (50.70%) - LeetSync

--- 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/minimum-number-of-increments-on-subarrays-to-form-a-target-array.java
+++ 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/minimum-number-of-increments-on-subarrays-to-form-a-target-array.java
@@ -1,24 +1,28 @@
 class Solution {
     public int minNumberOperations(int[] target) {
-        // int count=0;
-        // Arrays.sort(target);
-        // for(int i= target.length-1;i>=0;i--){
-        //     for(int j=i-1;j>=0;j--){
-        //         if(target[i]==target[j]) j--;
-        //         else {
-        //             count += target[i]-target[j];
-        //             i=j;
-        //         }
-        //     }
-        // }
-
-        // return count;
-
-        int count = target[0];
-        for(int i=1;i<target.length;i++){
-            count += Math.max(target[i]-target[i-1],0);
+        int count = target[target.length-1];
+        for(int i= target.length-1;i>=0;i--){
+            
+            for(int j = i-1;j>=0;j--){
+                if(target[i] < target[j]) {
+                     count += target[j]-target[i];
+                    i=j+1;
+                    break;
+                }
+                else {
+                   break;
+                }
+            
+            }
         }
 
         return count;
+
+        // int count = target[0];
+        // for(int i=1;i<target.length;i++){
+        //     count += Math.max(target[i]-target[i-1],0);
+        // }
+
+        // return count;
     }
 }
