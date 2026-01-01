3983e83e571fd2175e4b29f5a06417f6087c64d5
BUBALAN S <bubalanshanmugam62@gmail.com>
2026-01-01 14:00:25 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (7.44%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,18 +1,18 @@
 class Solution {
     public int[] plusOne(int[] d) {
-        int n = d.length;
-
-        for(int i =n-1; i>=0;i--){
-            if(d[i]<9){
-                d[i]++;
-                return d;
+    
+            for(int i = d.length-1; i >=0; i--){
+                if(d[i]!=9){
+                    d[i]+=1;
+                    return d;
+                }
+                d[i]=0;
             }
-            d[i]=0;
-        }
 
-        int[] result = new int[n+1];
+            d = new int[d.length+1];
 
-        result[0]=1;
-        return result;
+            d[0]=1;
+            
+            return d;
     }
 }
