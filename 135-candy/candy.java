be3d8893fe3cbbe05a20a78e65a990bf7fb01cd7
BUBALAN S <bubalanshanmugam62@gmail.com>
2026-01-14 23:45:09 +0530

    Time: 3 ms (89.24%) | Memory: 47.6 MB (57.87%) - LeetSync

--- 135-candy/candy.java
+++ 135-candy/candy.java
@@ -2,9 +2,7 @@ class Solution {
     public int candy(int[] ratings) {
         int sum =0;
         int [] left = new int[ratings.length];
-        int [] right = new int[ratings.length];
         left[0]=1;
-        right[ratings.length-1]=1;
         for(int i=1;i<ratings.length;i++){
             
             if(ratings[i]>ratings[i-1]){
@@ -13,20 +11,24 @@ class Solution {
                 left [i]=1;
             }
         }
-
+        int right =1;
+        sum += Math.max(1, left[ratings.length-1]);
+        int curr = 1;
         for(int i=ratings.length-2;i>=0;i--){
             
             if(ratings[i]>ratings[i+1]){
-                right[i]= right[i+1]+1;
+                curr=right+1;
+                right = curr;
             }else{
-                right [i]=1;
+                right=1;
             }
+            sum+= Math.max(left[i], right);
         }
 
 
-        for(int i=0;i<left.length;i++){
-            sum += Math.max(left[i], right[i]);
-        }
+        // for(int i=0;i<left.length;i++){
+        //     sum += left[i];
+        // }
 
         return sum;
     }
