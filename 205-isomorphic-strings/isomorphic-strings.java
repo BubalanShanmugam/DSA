5dd5b4ca1df4c4ef5ba1b11c763f234b396c5c6a
BUBALAN S <bubalanshanmugam62@gmail.com>
2026-01-16 15:33:59 +0530

    Time: 7 ms (76.61%) | Memory: 44.1 MB (20.02%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,26 +1,19 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
         if(s.length() != t.length()) return false;
-        int m1[] = new int[256];
-        int m2[] = new int[256];
-        int n = s.length();
-        for (int i = 0; i < n; ++i) {
-            if (m1[s.charAt(i)] != m2[t.charAt(i)]) return false;
-            m1[s.charAt(i)] = i + 1;
-            m2[t.charAt(i)] = i + 1;
+        int [] c1 = new int[256];
+        int [] c2 = new int[256];
+        
+        for(int i=0;i<s.length();i++){
+            if(c1[s.charAt(i)] == c2[t.charAt(i)]){
+                c1[s.charAt(i)]= i+1;
+                c2[t.charAt(i)]=i+1;
+            }
+            else{
+                return false;
+            }
         }
         return true;
     }
-    //     
-
-    //     HashSet<String> set1 = new HashSet<>();
-    //     HashSet<String> set2 = new HashSet<>();
-
-    //     for(int i=0; i<s.length();i++){
-    //         set1.add(s.charAt(i)+"");
-    //         set2.add(t.charAt(i)+"");
-    //     }
-
-    //     return set1.size()==set2.size();
-    // }
 }
+
