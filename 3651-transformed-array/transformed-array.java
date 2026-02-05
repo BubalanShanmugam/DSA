3447fc65a30409f428e2b3f30cbd5672d0d38428
BUBALAN S <bubalanshanmugam62@gmail.com>
2026-02-05 20:17:44 +0530

    Time: 1 ms (98.72%) | Memory: 46.9 MB (13.78%) - LeetSync

---
+++ 3651-transformed-array/transformed-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] constructTransformedArray(int[] nums) {
+        int n = nums.length;
+        int [] res = new int[n];
+
+        for(int i=0;i<n;i++){
+            // if(nums[i]>0){
+            //     res[i]= nums[(i+nums[i])%n];
+            // }
+            // else if(nums[i]<0){
+                res[i]= nums[ (n + ((i+nums[i])%n)) %n];
+            // }
+            // else{
+            //     res[i]=nums[i];
+            // }
+        }
+        return res;
+    }
+}
